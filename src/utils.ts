8f903af9024985496e07f6088685c1ffa21bec08
Bruno Carsten de Araújo <brunocarsten@gmail.com>
2024-05-27 23:36:09 -0300

    add routes array fix

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ import { defu } from 'defu'
 import type { Nuxt } from '@nuxt/schema'
 
 export function prepareNitroConfig(_nuxt: Nuxt, routes: string) {
-  _nuxt.options.nitro.prerender = defu(_nuxt.options.nitro.prerender, { crawlLinks: true, routes: [routes] })
+  _nuxt.options.nitro.prerender = defu(_nuxt.options.nitro.prerender, { crawlLinks: true, routes: [...routes] })
 }
